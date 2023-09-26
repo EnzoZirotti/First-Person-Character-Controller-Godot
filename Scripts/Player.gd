11eb19efc70afe1a4b90790ea84a491a777cf798
EnzoZirotti <zirotti32@gmail.com>
2023-09-25 23:13:53 -0600

    Finalizing the movement but still framey

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,6 +10,10 @@ const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
 
+
+#FOV Variables
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 9.8
 
@@ -66,6 +70,11 @@ func _physics_process(delta):
 #head bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
+	
+	# FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 	
 	move_and_slide()
 	
